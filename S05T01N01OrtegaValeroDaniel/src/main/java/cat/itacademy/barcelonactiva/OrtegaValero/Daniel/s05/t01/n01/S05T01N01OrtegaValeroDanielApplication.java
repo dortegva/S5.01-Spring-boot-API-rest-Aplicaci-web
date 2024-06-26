c69c872500223cd24f43c56942d373857fd2ed62
dortegva <159772830+dortegva@users.noreply.github.com>
2024-06-26 21:51:25 +0200

    Add files via upload

---
+++ S05T01N01OrtegaValeroDaniel/src/main/java/cat/itacademy/barcelonactiva/OrtegaValero/Daniel/s05/t01/n01/S05T01N01OrtegaValeroDanielApplication.java
@@ -0,0 +1,13 @@
+package cat.itacademy.barcelonactiva.OrtegaValero.Daniel.s05.t01.n01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class S05T01N01OrtegaValeroDanielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(S05T01N01OrtegaValeroDanielApplication.class, args);
+	}
+
+}
